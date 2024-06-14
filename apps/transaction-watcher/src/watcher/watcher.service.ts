e4e966baf9b4b9ba580318fef50241c634c3c54c
Alexander Velichkov <AVelichkov_c@spherasolutions.com>
2024-06-14 18:32:06 +0300

    Remove dead code

--- apps/transaction-watcher/src/watcher/watcher.service.ts
+++ apps/transaction-watcher/src/watcher/watcher.service.ts
@@ -1,14 +1,7 @@
 import { HttpService } from '@nestjs/axios';
 import { Injectable, OnModuleInit } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
-import {
-  InfuraEthGetFilterChangesResponse,
-  InfuraEthNewFilterResponse,
-  InfuraMethod,
-  InfuraRequestType,
-} from '@nexo-monorepo/infura-shared';
 import { NexoTransactionWatcherConfiguration } from '@nexo-monorepo/nexo-transaction-watcher-api';
-import { lastValueFrom, map } from 'rxjs';
 import Web3 from 'web3';
 import { RegisteredSubscription } from 'web3/lib/commonjs/eth.exports';
 import { BlockHeaders } from '@nexo-monorepo/ethereum-shared';
@@ -66,36 +59,4 @@ export class WatcherService implements OnModuleInit {
       }
     });
   }
-
-  async track(): Promise<InfuraEthGetFilterChangesResponse> {
-    const newFilterData: InfuraRequestType = {
-      jsonrpc: '2.0',
-      id: 415, // The hell is that?
-      method: InfuraMethod.EthNewFilter,
-      params: [{}],
-    };
-
-    const newFilter = await lastValueFrom(
-      this.httpService
-        .post<InfuraEthNewFilterResponse>(this.INFURA_REQUEST_URL, newFilterData)
-        .pipe(map((res) => res?.data)),
-    );
-
-    const getFilterChangesData: InfuraRequestType = {
-      jsonrpc: '2.0',
-      id: 415, // The hell is that?
-      method: InfuraMethod.EthGetFilterChanges,
-      params: [newFilter.result],
-    };
-
-    const changes = await lastValueFrom(
-      this.httpService
-        .post<InfuraEthGetFilterChangesResponse>(this.INFURA_REQUEST_URL, getFilterChangesData)
-        .pipe(map((res) => res?.data)),
-    );
-
-    return changes;
-  }
-
-  async updateFilters(): Promise<void> {}
 }
